eb83202e42558c42fc5192a8da9b6b1c2821933b
Navilrc <imailnaveenaks@gmail.com>
2026-01-14 11:30:09 +0530

    work on add command logic

--- Backend/controllers/add.js
+++ Backend/controllers/add.js
@@ -1,4 +1,23 @@
-async function addRepo(){
-    console.log("Add command called");
+const fs = require("fs").promises;
+const path = require ("path");
+
+
+async function addRepo(filePath){
+const repoPath = path.resolve(process.cwd(), ".myGit");
+const stagingPath = path.join(repoPath, "staging");
+
+try {
+    await fs.mkdir(stagingPath , { recursive: true });
+    // Logic to add files to staging area would go here
+    const fileName = path.basename(filePath);
+    await fs.copyFile(filePath, path.join(stagingPath, fileName));
+    console.log(`Added ${fileName} to staging area.`);
+    
+} catch (error) {
+    console.error("Error adding files to staging:", error);
+    
+}
+
+   // console.log("Add command called");
 }
 module.exports = { addRepo };
