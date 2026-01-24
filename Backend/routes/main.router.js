fb3be5f7ac4b9092ec477e1b60f21188e12c10f4
Navilrc <imailnaveenaks@gmail.com>
2026-01-25 03:43:29 +0530

    worked on issue router and controllers

--- Backend/routes/main.router.js
+++ Backend/routes/main.router.js
@@ -1,11 +1,13 @@
 const espress = require ('express');
 const userRouter = require ('./user.router');
 const repoRouter = require ('./repo.router');
+const issueRouter = require ('./issue.router');
 
 const mainRouter = espress.Router();
 
 mainRouter.use(userRouter);
 mainRouter.use(repoRouter);
+mainRouter.use(issueRouter);
 
 mainRouter.get("/", (req, res) => {
     res.send("Welcome to the User Management System");
